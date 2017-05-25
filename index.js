cf5c027cab458f6d6d924693093894358e8d7fe6
joeSaad <yousseffhs@yahoo.com>
2017-05-25 13:37:08 -0500

    Changes to files
    - add scss styling to come along
    - add option of knowing current installed version
    - cleaned some code

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ export default class xxxxx extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="xxxxx">
         xxxxx component
       </div>
     );
@@ -24,7 +24,10 @@ export default class xxxxx extends Component {
 }
 `;
 
-var scssContent = "";
+var scssContent = `.xxxxx{
+
+  }
+`;
 
 
 function capitalizeFirstLetter(s) {
@@ -32,51 +35,53 @@ function capitalizeFirstLetter(s) {
   return p.charAt(0).toUpperCase() + p.slice(1);
 }
 
-const answer = capitalizeFirstLetter(process.argv.slice(2, 3));
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+function main() {
+  if (process.argv.includes('-v')) {
+    console.log(require('./package.json').version)
+    return;
+  }
+  const answer = capitalizeFirstLetter(process.argv.slice(2, 3));
 
+  const dirPresent = process.argv.slice(3);
 
-const dirPresent = process.argv.slice(3);
+  const currentDir = dirPresent == '-d' ? `${answer}/` : '';
 
-const currentDir = dirPresent == '-d' ? `${answer}/` : '';
+  const filesToCreate = ['.js', '.scss'];
+  const filesContents = [jsxContent, scssContent];
 
-const filesToCreate = ['.js', '.scss'];
-const filesContents = [jsxContent, scssContent];
+  function createFiles() {
+    fs.writeFile(`./${currentDir}${answer}.scss`, filesContents[1].replace(/xxxxx/g, answer), function(err) {
+      if (err) {
+        return console.log(err);
+      } else {
+        console.log(`${answer}.scss saved successfully`.cyan);
+      }
+    });
+    fs.writeFile(`./${currentDir}${answer}.js`, filesContents[0].replace(/xxxxx/g, answer), function(err) {
+      if (err) {
+        return console.log(err);
+      } else {
+        console.log(`${answer}.js saved successfully`.cyan);
+      }
+    });
+  }
 
-function createFiles() {
-  fs.writeFile(`./${currentDir}${answer}.scss`, filesContents[1], function(err) {
-    if (err) {
-      return console.log(err);
-    } else {
-      console.log(`${answer}.scss saved successfully`.cyan);
-    }
-  });
-  fs.writeFile(`./${currentDir}${answer}.js`, filesContents[0].replace(/xxxxx/g, answer), function(err) {
-    if (err) {
-      return console.log(err);
-    } else {
-      console.log(`${answer}.js saved successfully`.cyan);
-    }
-  });
+  if (dirPresent == '-d') {
+    mkdirp('./' + answer, function(err) {
+      if (err) {
+        throw err;
+      }
+      // done
+      console.log(`Component ${answer} created successfully!`.green);
+    });
+
+    createFiles();
+
+  } else {
+    createFiles();
+  }
 }
 
-if (dirPresent == '-d') {
-  mkdirp('./' + answer, function(err) {
-    if (err) {
-      throw err;
-    }
-    // done
-    console.log(`Component ${answer} created successfully!`.green);
-  });
 
-  createFiles();
-
-} else {
-  createFiles();
-}
-
-rl.close();
+main();
